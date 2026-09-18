1d3af6a05e4fef5eace6d48b7ffc05f925413044
Conor Shaughnessy <G00432599@atu.ie>
2026-09-18 09:52:34 +0100

    Initial SpringBoot Catalog Service

---
+++ src/main/java/ie/atu/CICD/catalog/CiCd1CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.CICD.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCd1CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCd1CatalogServiceApplication.class, args);
+    }
+
+}
