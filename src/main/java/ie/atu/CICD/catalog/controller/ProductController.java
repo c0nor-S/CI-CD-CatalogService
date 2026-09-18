0ffbd6973015bc2041fca229ac49b165408be086
Conor Shaughnessy <G00432599@atu.ie>
2026-09-18 10:23:41 +0100

    Added Post & Get For Product Controller

--- src/main/java/ie/atu/CICD/catalog/controller/ProductController.java
+++ src/main/java/ie/atu/CICD/catalog/controller/ProductController.java
@@ -1,4 +1,26 @@
 package ie.atu.CICD.catalog.controller;
 
+import ie.atu.CICD.catalog.model.Product;
+import ie.atu.CICD.catalog.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getAll();
+    }
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.create(product);
+    }
 }
